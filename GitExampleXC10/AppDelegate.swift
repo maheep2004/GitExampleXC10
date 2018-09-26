3e4e58071cc6f8dcb7eec4f6dca7f22019622b6f
Maheep Wadhwa <maheep2004@gmail.com>
2018-09-26 14:35:29 -0400

    Initial Commit

---
+++ GitExampleXC10/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC10
+//
+//  Created by Maheep Wadhwa on 2018-09-26.
+//  Copyright © 2018 Maheep Wadhwa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExampleXC10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC10
+//
+//  Created by Maheep Wadhwa on 2018-09-26.
+//  Copyright © 2018 Maheep Wadhwa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
